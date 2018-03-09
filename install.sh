ff9efe2469fbcf702f1abf5a58b0da0f3e907d61
Dan McInerney <danhmcinerney@gmail.com>
2018-03-08 17:59:52 -0700

    updated install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@ apt-get update
 apt-get install git python-pip
 pip2 install pipenv
 pipenv install --two
-pipenv shell
-echo '[*] Run the following commands:'
-echo '      git clone https://github.com/DanMcInerney/msfrpc'
-echo '      cd msfrpc && python2 setup install && cd ..'
 echo '[*] Run metasploit in a new terminal using the included rc file:'
 echo '      sudo msfconsole -r msfrpc.rc'
+echo '[*] Create virtualenv:'
+echo '      pipenv shell'
+echo '[*] Run the following commands in the virtualenv:'
+echo '      git clone https://github.com/DanMcInerney/msfrpc'
+echo '      cd msfrpc/python-msfrpc/ && python2 setup install && cd ../..'
 echo '[*] Run script:'
 echo '      ./msf-autopwn -l targets.txt'
