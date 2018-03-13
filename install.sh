585f4f8b68a886d8f3160e05e651dffcd8e6afff
Dan <danhmcinerney@gmail.com>
2018-03-13 14:02:19 -0600

    updated install

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ apt-get update
 apt-get install git python-pip
 pip2 install pipenv
 pipenv install --two
-pipenv shell
 echo '[*] Run the following commands:'
 echo '      git clone https://github.com/DanMcInerney/msfrpc'
 echo '      cd msfrpc && python2 setup install && cd ..'
